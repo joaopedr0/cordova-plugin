9ae9490d206828dc17cc20484e48ae2e33eb3ede
João Pedro <joaopedr0@users.noreply.github.com>
2020-04-14 15:38:21 -0300

    Correção

--- src/android/KioskActivity.java
+++ src/android/KioskActivity.java
@@ -111,13 +111,13 @@ public class KioskActivity extends CordovaActivity {
             am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
 
             // sometime required to close opened notification area
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask(){
-                public void run() {
-                    Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-                    sendBroadcast(closeDialog);
-                }
-            }, 500); // 0.5 second
+            // Timer timer = new Timer();
+            // timer.schedule(new TimerTask(){
+            //     public void run() {
+            //         Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            //         sendBroadcast(closeDialog);
+            //     }
+            // }, 500); // 0.5 second
         }
     }
 }
