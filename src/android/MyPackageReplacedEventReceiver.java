568ce224307343e7dd87b5bcdd3a22f7613c4f77
João <joao.souza@digicade.com.br>
2020-08-24 10:06:58 -0300

    teste power button

--- src/android/MyPackageReplacedEventReceiver.java
+++ src/android/MyPackageReplacedEventReceiver.java
@@ -8,11 +8,39 @@ import android.content.Intent;
  * Starts {@link HomeActivity} after the app APK is updated.
  */
 public class MyPackageReplacedEventReceiver extends BroadcastReceiver {
+    private static final String PREF_KIOSK_MODE = "pref_kiosk_mode";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        if(Intent.ACTION_SCREEN_OFF.equals(intent.getAction())){
+            AppContext ctx = (AppContext) context.getApplicationContext();
+            // is Kiosk Mode active?
+            if(isKioskModeActive(ctx)) {
+                wakeUpDevice(ctx);
+            }
+        }
         System.out.println("Kiosk application restarting after upgrade");
         Intent newIntent = new Intent(context, KioskActivity.class);
         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(newIntent);
+
+    }
+
+    private void wakeUpDevice(AppContext context) {
+        PowerManager.WakeLock wakeLock = context.getWakeLock(); // get WakeLock reference via AppContext
+        if (wakeLock.isHeld()) {
+          wakeLock.release(); // release old wake lock
+        }
+    
+        // create a new wake lock...
+        wakeLock.acquire();
+    
+        // ... and release again
+        wakeLock.release();
+    }
+    
+    private boolean isKioskModeActive(final Context context) {
+    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+    return sp.getBoolean(PREF_KIOSK_MODE, false);
     }
 }
